339f71a989ab04770d836eadf5dfc0e7db9b4863
crossdia <tokyosta@gmail.com>
2017-12-19 18:38:35 +0900

    permitted parameter for sign up
    Correct permitted parameter setting for  devise controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [ :username, :email, :password, :password_confirmation　]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+    devise_parameter_sanitizer.for(:sign_up).push(:username)
   end
-
 
 end
